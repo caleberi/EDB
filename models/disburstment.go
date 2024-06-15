227d3fef767f2cb18d2d9ecf353bcf80d5f1bbd3
caleberi <calerioluwa@gmail.com>
2024-06-16 00:50:42 +0100

    feat: add model for disbursment

---
+++ models/disburstment.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Sender struct {
+	Name     string `json:"name"`
+	Country  string `json:"country"`
+	Phone    string `json:"phone"`
+	Address  string `json:"address"`
+	DOB      string `json:"dob"`
+	Email    string `json:"email"`
+	IDNumber string `json:"idNumber"`
+	IDType   string `json:"idType"`
+}
+
+type Destination struct {
+	AccountName   string `json:"accountName"`
+	AccountNumber string `json:"accountNumber"`
+	AccountType   string `json:"accountType"`
+	NetworkID     string `json:"networkId"`
+}
+
+type Payment struct {
+	ID              string      `json:"id"`
+	ChannelID       string      `json:"channelId"`
+	SequenceID      string      `json:"sequenceId"`
+	Currency        string      `json:"currency"`
+	Country         string      `json:"country"`
+	Amount          float64     `json:"amount"`
+	Reason          string      `json:"reason"`
+	ConvertedAmount float64     `json:"convertedAmount"`
+	Status          string      `json:"status"`
+	Rate            float64     `json:"rate"`
+	Sender          Sender      `json:"sender"`
+	Destination     Destination `json:"destination"`
+	CreatedAt       string      `json:"createdAt"`
+	UpdatedAt       string      `json:"updatedAt"`
+	ExpiresAt       string      `json:"expiresAt"`
+}
+
+type Disbursement struct {
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty" validate:"required"`
+	ReceiverID   primitive.ObjectID `bson:"receiver_id,omitempty" json:"receiver_id,omitempty" validate:"required"`
+	CreatedAt    time.Time          `bson:"created_at,omitempty" json:"created_at,omitempty" validate:"required"`
+	UpdatedAt    time.Time          `bson:"updated_at,omitempty" json:"updated_at,omitempty" validate:"required"`
+	SalaryAmount float64            `bson:"salary_amount,omitempty" json:"salary_amount,omitempty" validate:"required"`
+	SenderID     primitive.ObjectID `bson:"sender_id,omitempty" json:"sender_id,omitempty" validate:"required"`
+	Status       string             `bson:"status,omitempty" json:"status,omitempty" validate:"required"`
+	Payment      Payment            `bson:"payment,omitempty" json:"payment,omitempty" validate:"required"`
+}
