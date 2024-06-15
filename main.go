41157037c62213d9f3e0db22d284714c3dcedf22
caleberi <calerioluwa@gmail.com>
2024-06-16 00:59:40 +0100

    feat: add main entry

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+	"yc-backend/common"
+	"yc-backend/engine"
+	"yc-backend/internals"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	config, err := common.LoadConfiguration(common.ConfEnvSetting{YamlFilePath: []string{"./dev.yml"}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger := internals.GetLogger()
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+	defer serverStopCtx()
+	if config.LogLevel == "debug" {
+		logger.Infof("[Gin-Debug] SET gin.forceConsoleLog")
+		gin.ForceConsoleColor()
+	}
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancelFunc()
+	clientOpts := options.Client().
+		ApplyURI(config.MongoDB.DBUri).
+		SetMaxPoolSize(20).
+		SetMinPoolSize(5)
+	client, err := mongo.Connect(ctx, clientOpts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	app := &engine.Application{
+		DB:      client,
+		Config:  config,
+		Logger:  logger,
+		Context: serverCtx,
+	}
+
+	app.Setup().
+		RegisterRoute().
+		GracefulShutdown()
+
+	if err := app.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+}
